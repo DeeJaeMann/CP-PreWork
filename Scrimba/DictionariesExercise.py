610ce490306ef8bbf5dd4618485d210bd29973a2
Dee J. Mann <dee.j.mann@hotmail.com>
2024-01-13 14:55:35 -0500

    Corrected variable casing

--- Scrimba/DictionariesExercise.py
+++ Scrimba/DictionariesExercise.py
@@ -30,52 +30,52 @@
 # python 'lore'
 
 # create stores
-dictFreelancers = {'name':'freelancing Shop','brian': 70, 'black knight':20, 'biccus diccus':100, 'grim reaper':500, 'minstrel':-15}
-dictAntiques = {'name':'Antique Shop','french castle':400, 'wooden grail':3, 'scythe':150, 'catapult':75, 'german joke':5}
-dictPetShop = {'name':'Pet Shop','blue parrot':10, 'white rabbit':5, 'newt': 2}
+dct_freenlancers = {'name':'freelancing Shop','brian': 70, 'black knight':20, 'biccus diccus':100, 'grim reaper':500, 'minstrel':-15}
+dct_antiques = {'name':'Antique Shop','french castle':400, 'wooden grail':3, 'scythe':150, 'catapult':75, 'german joke':5}
+dct_pet_shop = {'name':'Pet Shop','blue parrot':10, 'white rabbit':5, 'newt': 2}
 
 #create an dempty shopping cart
-dictCart = {}
-intWallet = 1000
-strBuyItemList = ""
+dct_cart = {}
+int_wallet = 1000
+str_buy_item_list = ""
 
 # Beginning inventory
-dictDepartmentStoreBegin = {}
-for strDepartment in (dictFreelancers, dictAntiques, dictPetShop) : 
-    dictDepartmentStoreBegin.update(strDepartment)
-dictDepartmentStoreBegin.pop('name')
+dct_dept_store_begin = {}
+for str_dept in (dct_freenlancers, dct_antiques, dct_pet_shop) : 
+    dct_dept_store_begin.update(str_dept)
+dct_dept_store_begin.pop('name')
 
-print(f"Morning inventory: {sorted(dictDepartmentStoreBegin.items())}")
+print(f"Morning inventory: {sorted(dct_dept_store_begin.items())}")
 print(f"**********")
 #loop through stores/dicts
-for strShop in (dictFreelancers, dictAntiques, dictPetShop) :
+for str_shop in (dct_freenlancers, dct_antiques, dct_pet_shop) :
 
     #inputbox  to show what you can buy...capture textstring of what was bought...make lowercase
-    strBuyItem = input(f"Welcome to {strShop['name'].title()}! (type exit to exit store) What do you want to buy: \n{strShop}\nItem: ").lower()
+    str_buy_item = input(f"Welcome to {str_shop['name'].title()}! (type exit to exit store) What do you want to buy: \n{str_shop}\nItem: ").lower()
     # v1.2 code
     # exit on exit or buying non existant item
-    if(strBuyItem == "exit") :
+    if(str_buy_item == "exit") :
         continue
-    elif strBuyItem not in strShop :
+    elif str_buy_item not in str_shop :
         continue
     # to make output pretty
-    elif (len(strBuyItemList) > 0) : 
-        strBuyItemList += ", "
+    elif (len(str_buy_item_list) > 0) : 
+        str_buy_item_list += ", "
 
-    strBuyItemList += f"{strBuyItem.title()} : {strShop[strBuyItem]} gp"
+    str_buy_item_list += f"{str_buy_item.title()} : {str_shop[str_buy_item]} gp"
     #update the cart
-    dictCart.update({strBuyItem:strShop.pop(strBuyItem)}) # use pop...
+    dct_cart.update({str_buy_item:str_shop.pop(str_buy_item)}) # use pop...
     # variables for string output
 #strItemsList = ", ".join(list(dictCart.keys()))
-intCartTotal = sum(dictCart.values())
-intWallet = intWallet - intCartTotal
+int_cart_total = sum(dct_cart.values())
+int_wallet = int_wallet - int_cart_total
 
-print(f"You Purchased: {strBuyItemList}. Your total is {intCartTotal} gp.  Your change is {intWallet} gp. Have a nice day of mayhem!")
+print(f"You Purchased: {str_buy_item_list}. Your total is {int_cart_total} gp.  Your change is {int_wallet} gp. Have a nice day of mayhem!")
 # End inventory
-dictDepartmentStoreEnd = {}
-for strDepartment in (dictFreelancers, dictAntiques, dictPetShop) : 
-    dictDepartmentStoreEnd.update(strDepartment)
-dictDepartmentStoreEnd.pop('name')
+dct_dept_store_end = {}
+for str_dept in (dct_freenlancers, dct_antiques, dct_pet_shop) : 
+    dct_dept_store_end.update(str_dept)
+dct_dept_store_end.pop('name')
 
 print(f"**********")
-print(f"Evening inventory: {sorted(dictDepartmentStoreEnd.items())}")
+print(f"Evening inventory: {sorted(dct_dept_store_end.items())}")
