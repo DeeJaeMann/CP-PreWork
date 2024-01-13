c4cfef52f7e376165afeff7a05c80732351f4340
Dee J. Mann <dee.j.mann@hotmail.com>
2024-01-13 15:14:03 -0500

    Corrected variable casing

--- Scrimba/ZipUnzip.py
+++ Scrimba/ZipUnzip.py
@@ -1,6 +1,6 @@
-listNums = [1,2,3,4] 
-listLetters = ['a','b','c','d']
-listNames =['John','Eric','Michael','Graham','Joe']
-listCombo = list(zip(listNums, listLetters))
-print(listCombo)
+lst_nums = [1,2,3,4] 
+lst_letters = ['a','b','c','d']
+lst_names =['John','Eric','Michael','Graham','Joe']
+lst_combo = list(zip(lst_nums, lst_letters))
+print(lst_combo)
 #unzip with zip(*...)
