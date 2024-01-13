9a9ddb6c68e4215b1effa4b205509c45de0b91a8
Dee J. Mann <dee.j.mann@hotmail.com>
2024-01-13 14:47:50 -0500

    Corrected vairable casing

--- Scrimba/CalculatorExercise.py
+++ Scrimba/CalculatorExercise.py
@@ -3,24 +3,24 @@
 # Bonus: Extend functionality with extra mode so it also does celsius to fahrenheit conversion
 # formula is: temp in C*9/5 + 32 = temp in f
 
-def PerformCalculation(fltFirstNumber, fltSecondNumber=0, strOperation = "+") :
-    if(strOperation == "+") :
-        return fltFirstNumber + fltSecondNumber
-    elif(strOperation == "-") :
-        return fltFirstNumber - fltSecondNumber
-    elif(strOperation == "*") :
-        return fltFirstNumber * fltSecondNumber
-    elif(strOperation == "/") :
-        return fltFirstNumber / fltSecondNumber
-    elif(strOperation == "f") :
-        return (f"{fltFirstNumber * 9/5 + 32} degrees fahrenheight")
+def PerformCalculation(flt_first_number, flt_second_number=0, str_operation = "+") :
+    if(str_operation == "+") :
+        return flt_first_number + flt_second_number
+    elif(str_operation == "-") :
+        return flt_first_number - flt_second_number
+    elif(str_operation == "*") :
+        return flt_first_number * flt_second_number
+    elif(str_operation == "/") :
+        return flt_first_number / flt_second_number
+    elif(str_operation == "f") :
+        return (f"{flt_first_number * 9/5 + 32} degrees fahrenheight")
     else :
         return "Invalid Operation"
     
-strOperationInput = input("Please enter an operation (+, -, *, /, f (C to F conversion)): ")
-fltFirstInput = float(input(f"Enter first number (operation {strOperationInput}): "))
-if(strOperationInput != "f") :
-    fltSecondInput = float(input(f"Enter second number (operation {strOperationInput}): "))
+str_operation_input = input("Please enter an operation (+, -, *, /, f (C to F conversion)): ")
+fltFirstInput = float(input(f"Enter first number (operation {str_operation_input}): "))
+if(str_operation_input != "f") :
+    flt_second_input = float(input(f"Enter second number (operation {str_operation_input}): "))
 else :
-    fltSecondInput = 0
-print(f"The answer is: {PerformCalculation(fltFirstInput, fltSecondInput, strOperationInput)}")
+    flt_second_input = 0
+print(f"The answer is: {PerformCalculation(fltFirstInput, flt_second_input, str_operation_input)}")
