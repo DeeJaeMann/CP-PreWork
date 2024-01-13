ca5eadcd0aeff285af75ee854582798f4240159e
Dee J. Mann <dee.j.mann@hotmail.com>
2024-01-13 15:11:35 -0500

    Corrected variable casing

--- Scrimba/TryExcept.py
+++ Scrimba/TryExcept.py
@@ -8,17 +8,17 @@
     #always executed 
 
 try :
-    intNum = int(input("Enter a number between 1 and 30: "))
-    intNumResult = 30/intNum
-    if(intNum > 30):
-       raise ValueError(intNum)
+    int_num = int(input("Enter a number between 1 and 30: "))
+    int_num_result = 30/int_num
+    if(int_num > 30):
+       raise ValueError(int_num)
 except ZeroDivisionError as err:
     print(err, "You can't divide by Zero!!!")
 except ValueError as err:
-    print(err, intNum, "Bad Value Not Between 1 and 30!")
+    print(err, int_num, "Bad Value Not Between 1 and 30!")
 except:
     print("Invalid Input!")
 else:
-    print(f"30 divided by {intNum} is: {intNumResult}")
+    print(f"30 divided by {int_num} is: {int_num_result}")
 finally:
     print("**Thank you for playing!**")
