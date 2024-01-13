744195d4989b479ed5588449747ea06c1bfbf75e
Dee J. Mann <dee.j.mann@hotmail.com>
2024-01-13 15:07:24 -0500

    Corrected variable casing

--- Scrimba/MathTutor.py
+++ Scrimba/MathTutor.py
@@ -23,75 +23,75 @@
 # We're going to be using random numbers so import the module
 import random
 # Bonus 1 - Import the time module
-from time import time as myTime
+from time import time as my_time
 
-intScore = 0
-intCorrect = 0
-intMaxQuestions = 1
+int_score = 0
+int_correct = 0
+int_max_questions = 1
 # Default max multiplier to 10
-intMaxMultiplier = 10
+int_max_multiplier = 10
 # For Bonus #2 - Limit how low the multiplier can be
-intMinMultiplier = 2
+int_min_multiplier = 2
 # For Bonus #3 - Show all questions and answers at end
-dictResultsSheet = {}
-strAnswers = "*****\nAnswers\n*****\n"
+dct_results_sheet = {}
+str_answers = "*****\nAnswers\n*****\n"
 # Get the user input and typecast to int
-intMaxQuestions = int(input("Enter the number of practice questions: "))
-intMaxMultiplier = int(input("Enter the largest number to multiply by: "))
+int_max_questions = int(input("Enter the number of practice questions: "))
+int_max_multiplier = int(input("Enter the largest number to multiply by: "))
 
 # Bonus 1 - Record start time
-timeStart = myTime()
+time_start = my_time()
 
 # Thank you message
-strThankYou = "Thank you for playing!"
+str_thank_you = "Thank you for playing!"
 
 # Check if our Multiplier entered is greater than our minimum allowed
-if(intMaxMultiplier >= intMinMultiplier) :
+if(int_max_multiplier >= int_min_multiplier) :
     # Loop to prompt for questions
-    for intQuestion in range(intMaxQuestions) :
+    for int_question in range(int_max_questions) :
         # Random First number
-        intFirstMult = random.randint(0,intMaxMultiplier)
+        int_first_mult = random.randint(0,int_max_multiplier)
         # Random Second number
-        intSecondMult = random.randint(0,intMaxMultiplier)
+        int_second_mult = random.randint(0,int_max_multiplier)
         # Calculate our answer
-        intAnswer = intFirstMult * intSecondMult
+        int_answer = int_first_mult * int_second_mult
 
 
 
         # Prompt for the question using a formatted string
-        intThisQuestion = int(input(f"Question {intQuestion + 1}: {intFirstMult} X {intSecondMult} = "))
+        int_this_question = int(input(f"Question {int_question + 1}: {int_first_mult} X {int_second_mult} = "))
 
         # Bonus 3 - Add this question to our dictionary
-        dictResultsSheet[f"Question {intQuestion+1}"] = f"{intFirstMult} X {intSecondMult} = {intAnswer} (You: {intThisQuestion})"
+        dct_results_sheet[f"Question {int_question+1}"] = f"{int_first_mult} X {int_second_mult} = {int_answer} (You: {int_this_question})"
 
         # Check if the entered answer is correct
-        if(intThisQuestion == intAnswer) :
-            intCorrect += 1
+        if(int_this_question == int_answer) :
+            int_correct += 1
 
     # Bonus 1 - Record end time
-    timeEnd = myTime()
+    time_end = my_time()
     # Bonus 3 - Formatting the string for pretty print
     # Loop through the keys and append the key and value to strAnswers in a pretty way
-    for strKey in dictResultsSheet.keys() :
-        strAnswers += f"{strKey} : {dictResultsSheet.get(strKey)}\n"
+    for strKey in dct_results_sheet.keys() :
+        str_answers += f"{strKey} : {dct_results_sheet.get(strKey)}\n"
 
-    strAnswers += "*****"
+    str_answers += "*****"
 
     # Update our score (percentage correct)
-    intScore = int((intCorrect / intMaxQuestions) * 100)
+    int_score = int((int_correct / int_max_questions) * 100)
     # Bonus 1 - Calculate our time difference
-    timeDiff = timeEnd - timeStart
+    time_diff = time_end - time_start
     # Bonus 1 - Calculate the average time per question
-    timeAvg = round(timeDiff/intMaxQuestions, 2)
+    time_avg = round(time_diff/int_max_questions, 2)
     # Format our results strings
-    strResults = f"Correct Answers: {intCorrect} / Total Questions: {intMaxQuestions}"
-    strPercentage = f"{intScore}% Correct"
+    str_results = f"Correct Answers: {int_correct} / Total Questions: {int_max_questions}"
+    str_percentage = f"{int_score}% Correct"
     # Bonus 1 - Output the time difference
-    strTimeResult = f"{round(timeDiff,2)} seconds (Average of {timeAvg} seconds per question)"
+    str_time_result = f"{round(time_diff,2)} seconds (Average of {time_avg} seconds per question)"
 
     # print(f"\n{strThankYou}\n*****\n{dictResultsSheet}\n*****\n{strResults}\n{strPercentage}")
     # The final output message including all of our information
-    print(f"\n{strThankYou}\n{strAnswers}\n{strResults}\n{strPercentage} in {strTimeResult}")
+    print(f"\n{str_thank_you}\n{str_answers}\n{str_results}\n{str_percentage} in {str_time_result}")
 else :
     # The number entered was lower than our minimum multiplier
-    print(f"Please enter a number greater than {intMinMultiplier-1}.")
+    print(f"Please enter a number greater than {int_min_multiplier-1}.")
